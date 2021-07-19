fd541b48790a8fff5dddc7fab7e1bfa529fc2cdb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-19 19:30:38 +0000

    Deploy michelleamesquita/demo-app to github.com/michelleamesquita/demo-app.git:gh-pages

---
+++ precache-manifest.c3d64d4596117bca894c14f546f80c7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "309a2c3875a71cfb79b7",
+    "url": "/demo-app/static/css/main.f2c199df.chunk.css"
+  },
+  {
+    "revision": "309a2c3875a71cfb79b7",
+    "url": "/demo-app/static/js/main.309a2c38.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "30b6f35d998e177353bdf6ce91f079a2",
+    "url": "/demo-app/index.html"
+  }
+];
